b6fbf6308d10e74b4eaa42bc85c79a700a8ef6e5
pedoc <pedoc@qq.com>
2025-04-12 13:22:39 +0800

    额外打开更多工具

--- build_busybox.sh
+++ build_busybox.sh
@@ -21,7 +21,21 @@ cd "$SRC_DIR"
 
 make distclean
 make defconfig
+
 sed -i 's/^# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
+sed -i 's/^# CONFIG_MINIPS is not set/CONFIG_MINIPS=y/' .config
+sed -i 's/^# CONFIG_NUKE is not set/CONFIG_NUKE=y/' .config
+sed -i 's/^# CONFIG_FEATURE_VI_REGEX_SEARCH is not set/CONFIG_FEATURE_VI_REGEX_SEARCH=y/' .config
+sed -i 's/^# CONFIG_TUNE2FS is not set/CONFIG_TUNE2FS=y/' .config
+sed -i 's/^# CONFIG_FEATURE_MOUNT_HELPERS is not set/CONFIG_FEATURE_MOUNT_HELPERS=y/' .config
+sed -i 's/^# CONFIG_FEATURE_MOUNT_NFS is not set/CONFIG_FEATURE_MOUNT_NFS=y/' .config
+sed -i 's/^# CONFIG_INOTIFYD is not set/CONFIG_INOTIFYD=y/' .config
+sed -i 's/^# CONFIG_RFKILL is not set/CONFIG_RFKILL=y/' .config
+sed -i 's/^# CONFIG_SETENFORCE is not set/CONFIG_SETENFORCE=y/' .config
+sed -i 's/^# CONFIG_GETENFORCE is not set/CONFIG_GETENFORCE=y/' .config
+sed -i 's/^# CONFIG_TUNE2FS is not set/CONFIG_TUNE2FS=y/' .config
+sed -i 's/^# CONFIG_FDISK_SUPPORT_LARGE_DISKS is not set/CONFIG_FDISK_SUPPORT_LARGE_DISKS=y/' .config
+
 yes "" | make oldconfig
 make -j$(nproc)
 
